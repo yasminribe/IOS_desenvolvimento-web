82849b35946135979fa272c1673f9bdc65f97944
Yasmin Ribeiro <yasmin.5126c@gmail.com>
2022-10-17 11:32:48 -0300

    DOM

--- JavaScript/DOM Aula1/imc.js
+++ JavaScript/DOM Aula1/imc.js
@@ -11,7 +11,7 @@ function imc(){
         const valorIMC = (peso.value / (altura.value**2)).toFixed(1)
 
         if(valorIMC <= 18.5){
-            resultado.innerHTML = `${nome.value}, seu IMC: ${valorIMC} Você está abaixo do peso`
+            resultado.innerHTML = `${nome.value}, seu IMC: ${valorIMC} <br> Você está abaixo do peso`
         }else if (valorIMC > 18.5 && valorIMC <= 24.9){
             resultado.innerHTML = `${nome.value}, seu IMC: ${valorIMC} Você está no peso Normal`
         }else if (valorIMC > 24.9 && valorIMC <= 29.9){
@@ -20,8 +20,11 @@ function imc(){
             resultado.innerHTML = `${nome.value}, seu IMC: ${valorIMC} Você está com Obesidade grau I `
         }else if (valorIMC > 34.9 && valorIMC <= 39.9){
             resultado.innerHTML = `${nome.value}, seu IMC: ${valorIMC} Você está com Obesidade grau II `
-    }else{
-        resultado.innerHTML = `${nome.value}, seu IMC: ${valorIMC} Você está com Obesidade grau III `
+        }else{
+            resultado.innerHTML = `${nome.value}, seu IMC: ${valorIMC} Você está com Obesidade grau III `
         }
+
+    }else{
+        alert('preencha o campo vazio')
     } 
 }
