9aca6706a52d481dba5a6bf44b2a11328fea023a
Yasmin Ribeiro <yasmin.5126c@gmail.com>
2022-10-19 09:37:30 -0300

    loops

---
+++ JavaScript/Atividade/exercicios04.js
@@ -0,0 +1,23 @@
+// faça um programa de que exiba os números de 50 a 100
+
+let num = 50 
+while(num <=100){
+    console.log(`Número: ${num}`)
+    num++
+}
+
+// faça um programa que escreva a contagem regressiva do lancamento de um foguete, o programa deve imprimir 10,9,8,7,....1,0
+let foguete = 10 
+while(foguete >=0){
+    console.log(`Contagem regressiva ${foguete}`)
+    foguete= foguete- 1
+}
+
+//faça um programa que exiba os pares de 0 a 20
+
+let pares = 0 
+
+while(pares <= 20){
+    console.log(pares)
+    pares = pares + 2
+}
---
+++ JavaScript/loops/script.js
@@ -0,0 +1,15 @@
+
+let passos = 0
+
+while (passos <= 3) {
+console.log(`Bom dia, 
+Passo ${passos}`)
+    passos++
+}
+
+let contador = 1 
+
+while (contador <=100){
+    console.log(contador)
+    contador++
+}
